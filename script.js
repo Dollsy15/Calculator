911e028a0434a3665ba9232dacce2c2fa9e535b9
Dollsy Rani <dollsyrani155@gmail.com>
2025-03-30 14:15:33 +0530

    starts

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ const minuteEl = document.querySelector('.minute');
 const updateTime = () => {
     const currentTime = new Date();
 
-    const currentHour = currentTime.getHours();
+    let currentHour = currentTime.getHours();
     const currentMinute = currentTime.getMinutes();
 
+    if(currentHour>12){
+        currentHour -= 12;
+    }
+
     hourEl.textContent = currentHour.toString();
-    minuteEl.textContent = currentMinute.toString();
+    minuteEl.textContent = currentMinute.toString().padStart(2,'0');
 }
+setInterval(updateTime, 1000);
+updateTime();
