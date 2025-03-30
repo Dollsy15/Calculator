a062f6c7274af4be969c88872f63cf97cb019444
Dollsy Rani <dollsyrani155@gmail.com>
2025-03-30 14:38:32 +0530

    calculator  parameters

--- script.js
+++ script.js
@@ -12,18 +12,18 @@ const multiplicationEl = document.querySelector('.multiplication');
 const divisionEl = document.querySelector('.division');
 const equalEl = document.querySelector('.equal');
 
-const updateTime = () => {
-    const currentTime = new Date();
-
-    let currentHour = currentTime.getHours();
-    const currentMinute = currentTime.getMinutes();
-
-    if(currentHour>12){
-        currentHour -= 12;
-    }
-
-    hourEl.textContent = currentHour.toString();
-    minuteEl.textContent = currentMinute.toString().padStart(2,'0');
-}
-setInterval(updateTime, 1000);
-updateTime();
+const decimalEl = document.querySelector('.decimal');
+const number0El = document.querySelector('.number-0');
+const number1El = document.querySelector('.number-1');
+const number2El = document.querySelector('.number-2');
+const number3El = document.querySelector('.number-3');
+const number4El = document.querySelector('.number-4');
+const number5El = document.querySelector('.number-5');
+const number6El = document.querySelector('.number-6');
+const number7El = document.querySelector('.number-7');
+const number8El = document.querySelector('.number-8');
+const number9El = document.querySelector('.number-9');
+const numberElArray = [
+  number0El, number1El, number2El, number3El, number4El,
+  number5El, number6El, number7El, number8El, number9El
+];
