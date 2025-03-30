18e4c0f885e61df7f6043b679b4dd759fe490830
Dollsy Rani <dollsyrani155@gmail.com>
2025-03-30 14:05:23 +0530

    start js

---
+++ script.js
@@ -0,0 +1,12 @@
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+
+const updateTime = () => {
+    const currentTime = new Date();
+
+    const currentHour = currentTime.getHours();
+    const currentMinute = currentTime.getMinutes();
+
+    hourEl.textContent = currentHour.toString();
+    minuteEl.textContent = currentMinute.toString();
+}
