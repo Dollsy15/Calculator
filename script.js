26fa6bdfb2dccc074b5d07bbdc61513eb1f523e6
Dollsy Rani <dollsyrani155@gmail.com>
2025-03-30 14:37:22 +0530

    more const parameters

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const acEl = document.querySelector('.ac');
 const pmEl = document.querySelector('.pm');
 const percentEl = document.querySelector('.percent');
 
+const additionEl = document.querySelector('.addition');
+const subtractionEl = document.querySelector('.subtraction');
+const multiplicationEl = document.querySelector('.multiplication');
+const divisionEl = document.querySelector('.division');
+const equalEl = document.querySelector('.equal');
+
 const updateTime = () => {
     const currentTime = new Date();
 
